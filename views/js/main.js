cd27c9aa2eb043d1b8d7e2c41b383b8fece035d0
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-12-21 19:01:35 -0800

    update determineDx position

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,11 @@ var resizePizzas = function(size) {
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     var pizzaContainers = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(pizzaContainers[0], size);
+
     for (var i = 0; i < pizzaContainers.length; i++) {
-      var dx = determineDx(pizzaContainers[i], size);
       var newwidth = (pizzaContainers[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      pizzaContainers[i].style.width = newwidth;
     }
   }
 
